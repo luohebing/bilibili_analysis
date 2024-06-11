dc5a8b50e45c57d32ca01b0bc3f169d70005020d
洛河冰 <41121178+luohebing@users.noreply.github.com>
2024-06-11 14:47:26 +0800

    发布

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # bilibili_analysis
- 毕业设计项目
+毕业设计项目，实现b站热门数据的关键词抽取与评论情感分析，使用Vue3+Flask+SQLite搭建，算法使用TF-IDF与Text-CNN实现。
+
+## 环境：
+```bash
+python 3.10
+Node.js v21.6.2
+npm 10.4.0
+```
+
+## 依赖：
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+cd bilibili_vite
+```
+
+```bash
+npm install
+```
+## 运行：
+
+```bash
+run_browser.bat
+run_server.bat
+```
+
+## 配置：
+在config.py中补全cookie以允许api访问。
--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # 定义 cookie
-cookie = '7efb9e9e%2C1723601124%2C655cb%2A21CjB8BpictfDua6kGIUfprlAArH6aJ0CSqPyBSni7OfdmdjtqGKrxkXoPQS1vS7GHNP8SVjdSN3ktWUNfTTBzbWQ2OW9mQVZFQkk4bXg4N0xTWEFCZzhVRmRqLUVmdDg1THd4aXJzdDJEWE1Db2FxaXdmdno2QkVGazR3aGFmVmxDNV9NVzdpUS13IIEC'  # 你的 Cookie，用于认证
+cookie = ''  # 你的 Cookie，用于认证
 
 headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
         "accept-language": "en,zh-CN;q=0.9,zh;q=0.8",
-        'Cookie': '7efb9e9e%2C1723601124%2C655cb%2A21CjB8BpictfDua6kGIUfprlAArH6aJ0CSqPyBSni7OfdmdjtqGKrxkXoPQS1vS7GHNP8SVjdSN3ktWUNfTTBzbWQ2OW9mQVZFQkk4bXg4N0xTWEFCZzhVRmRqLUVmdDg1THd4aXJzdDJEWE1Db2FxaXdmdno2QkVGazR3aGFmVmxDNV9NVzdpUS13IIEC'  # 使用 Cookie 认证
+        'Cookie': ''  # 使用 Cookie 认证
 }
 
 # 数据库
